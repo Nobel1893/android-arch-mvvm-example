d352d3542119d3a17453390e9112f83f4a2e419a
mohamed_nabil <m.nabil.fci2015@gmail.com>
2018-05-02 17:36:33 +0300

    changed Packages names .. added lang confings

---
+++ app/src/main/java/com/nobel/baseapp/MyApplication.java
@@ -0,0 +1,22 @@
+package com.nobel.baseapp;
+
+import com.nobel.baseapp.Base.NobelApplication;
+import com.nobel.baseapp.NobelProviders.PrefrencesProvider;
+
+
+/**
+ * Created by Mohamed Nabil Mohamed (Nobel) on 5/2/2018.
+ * byte code SA
+ * m.nabil.fci2015@gmail.com
+ */
+
+public class MyApplication extends NobelApplication {
+
+    PrefrencesProvider prefrencesProvider;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
